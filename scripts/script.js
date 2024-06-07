9d79d8369ed2a665a7a2add4db2aaa933b3d1804
kobelev anton <kobeleva166@gmail.com>
2024-06-07 23:09:35 +0500

    Сделал первый блок + его мобильную версию.

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,11 @@
 window.addEventListener('DOMContentLoaded', () => {
     const hamburger_menu = document.querySelector('.hamburger-menu');
     const nav_menu = document.querySelector('.header__nav');
+    const body = document.querySelector('body');
 
     hamburger_menu.addEventListener('click', () => {
         hamburger_menu.classList.toggle('hamburger-menu_active');
         nav_menu.classList.toggle('header__nav_active');
+        body.classList.toggle('body_active');
     })
 })
