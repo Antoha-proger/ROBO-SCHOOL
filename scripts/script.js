2c36a7c134bf4f634333ab704aa20d5c877b1801
kobelev anton <kobeleva166@gmail.com>
2024-06-08 21:49:36 +0500

    Сделал блок с тренерами.

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,23 @@
 "use strict"
 
+import Swiper from '../node_modules/swiper/swiper-bundle.min.mjs';
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    loop: true,
+    spaceBetween: 40,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    }
+
+});
+
 window.addEventListener('DOMContentLoaded', () => {
     const hamburger_menu = document.querySelector('.hamburger-menu');
     const nav_menu = document.querySelector('.header__nav');
