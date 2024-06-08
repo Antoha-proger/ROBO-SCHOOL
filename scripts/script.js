a934d06dcc9545043ea39a909f280b9ee5f1c656
kobelev anton <kobeleva166@gmail.com>
2024-06-08 15:24:41 +0500

    Сделал блок с информацией о школе.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+"use strict"
+
 window.addEventListener('DOMContentLoaded', () => {
     const hamburger_menu = document.querySelector('.hamburger-menu');
     const nav_menu = document.querySelector('.header__nav');
